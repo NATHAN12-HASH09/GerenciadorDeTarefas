2c80d80f4f30ff7139d94d38a8850dd8892cfebc
Nathan Borges <nathan@blsoftwares.com.br>
2025-11-30 22:58:14 -0300

    Implementação da tela de filtros das tarefas

---
+++ GerenciadorDeTarefas/Model/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorDeTarefas.Model
+{
+    public enum Status
+    {
+        Pendente = 0,
+        Finaliazado = 1,
+    }
+}
